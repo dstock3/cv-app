5f6c1c2ef1855e40e721c9d93a9e37e112bce5a4
David Stockdale <dstock3@gmail.com>
2022-02-10 21:46:20 -0600

    Added favicon.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ProfExp from './ProfExp'
 import GenInfo from './GenInfo'
 import Education from './Education'
 import Divider from './Divider'
+import Favicon from '../assets/favicon.png'
 
 const Resume = ({jobOne, jobTwo, jobThree, firstName, lastName, address, city, state, zip, email, phone, school, degree, year, honors, gpa, company, title, dutiesOne, dutiesTwo, dutiesThree, date, newCompanyTwo, newTitleTwo, newDutiesTwoOne, newDutiesTwoTwo, newDutiesTwoThree, newDateTwo, newCompanyThree, newTitleThree, newDutiesThreeOne, newDutiesThreeTwo, newDutiesThreeThree, newDateThree}) => {
-  return (
-    <section className="resume">
-        <h2 className="name">{firstName} {lastName}</h2>
 
-        <Divider />
+    useEffect(()=> {
+        const head = document.querySelector("head");
+        const favIcon = document.createElement("link")
+        head.appendChild(favIcon)
+        favIcon.rel = "shortcut icon";
+        favIcon.type = "image/png";
+        favIcon.href = Favicon
+    })
 
-        <GenInfo address={address} city={city} state={state} zip={zip} email={email} phone={phone} />
+    return (
+        <section className="resume">
+            <h2 className="name">{firstName} {lastName}</h2>
 
-        <ProfExp jobOne={jobOne} company={company} title={title} dutiesOne={dutiesOne} dutiesTwo={dutiesTwo} dutiesThree={dutiesThree} date={date} jobTwo={jobTwo} jobThree={jobThree} newCompanyTwo={newCompanyTwo} newTitleTwo={newTitleTwo} newDutiesTwoOne={newDutiesTwoOne} newDutiesTwoTwo={newDutiesTwoTwo} newDutiesTwoThree={newDutiesTwoThree} newDateTwo={newDateTwo} newCompanyThree={newCompanyThree} newTitleThree={newTitleThree} newDutiesThreeOne={newDutiesThreeOne} newDutiesThreeTwo={newDutiesThreeTwo} newDutiesThreeThree={newDutiesThreeThree} newDateThree={newDateThree}/>
-        
-        <Education school={school} degree={degree} year={year} honors={honors} gpa={gpa} />
+            <Divider />
 
-        <Divider />
-    </section>
-  )
+            <GenInfo address={address} city={city} state={state} zip={zip} email={email} phone={phone} />
+
+            <ProfExp jobOne={jobOne} company={company} title={title} dutiesOne={dutiesOne} dutiesTwo={dutiesTwo} dutiesThree={dutiesThree} date={date} jobTwo={jobTwo} jobThree={jobThree} newCompanyTwo={newCompanyTwo} newTitleTwo={newTitleTwo} newDutiesTwoOne={newDutiesTwoOne} newDutiesTwoTwo={newDutiesTwoTwo} newDutiesTwoThree={newDutiesTwoThree} newDateTwo={newDateTwo} newCompanyThree={newCompanyThree} newTitleThree={newTitleThree} newDutiesThreeOne={newDutiesThreeOne} newDutiesThreeTwo={newDutiesThreeTwo} newDutiesThreeThree={newDutiesThreeThree} newDateThree={newDateThree}/>
+            
+            <Education school={school} degree={degree} year={year} honors={honors} gpa={gpa} />
+
+            <Divider />
+        </section>
+    )
 }
 
 export default Resume
