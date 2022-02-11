f7f8c4c7831845ead1e0b55690414b5cefd52f83
David Stockdale <dstock3@gmail.com>
2022-02-10 21:09:54 -0600

    Converted ProfExp and Resume components into functional components.

--- src/components/ProfExp.js
+++ src/components/ProfExp.js
@@ -1,56 +1,55 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class ProfExp extends Component {
-    render() {
-        const {jobOne, jobTwo, jobThree, company, title, dutiesOne, dutiesTwo, dutiesThree, date, newCompanyTwo, newTitleTwo, newDutiesTwoOne, newDutiesTwoTwo, newDutiesTwoThree, newDateTwo, newCompanyThree, newTitleThree, newDutiesThreeOne, newDutiesThreeTwo, newDutiesThreeThree, newDateThree} = this.props
-        return (
-            <section className="experience">
-                <h3>Work Experience</h3>
-                <div className={jobOne}>
-                    <h4 className="title">{title}</h4>
-                    <div className="exp-subcontainer">
-                        <p className="company">{company}</p>
-                        <p className="date">{date}</p>
-                    </div>
+const ProfExp = ({jobOne, jobTwo, jobThree, company, title, dutiesOne, dutiesTwo, dutiesThree, date, newCompanyTwo, newTitleTwo, newDutiesTwoOne, newDutiesTwoTwo, newDutiesTwoThree, newDateTwo, newCompanyThree, newTitleThree, newDutiesThreeOne, newDutiesThreeTwo, newDutiesThreeThree, newDateThree}) => {
+  return (
+    <section className="experience">
 
-                    <ul className="duties">
-                        <li className="duties">{dutiesOne}</li>
-                        <li className="duties">{dutiesTwo}</li>
-                        <li className="duties">{dutiesThree}</li>
-                    </ul>
-                </div>
+        <h3>Work Experience</h3>
 
-                { jobTwo ?
-                    <div className={jobTwo}>
-                        <h4 className="title">{newTitleTwo}</h4>
-                        <p className="company">{newCompanyTwo}</p>
-                        <p className="date">{newDateTwo}</p>
+        <div className={jobOne}>
+            <h4 className="title">{title}</h4>
+            <div className="exp-subcontainer">
+                <p className="company">{company}</p>
+                <p className="date">{date}</p>
+            </div>
 
-                        <ul className="duties">
-                            <li className="duties">{newDutiesTwoOne}</li>
-                            <li className="duties">{newDutiesTwoTwo}</li>
-                            <li className="duties">{newDutiesTwoThree}</li>
-                        </ul>
-                    </div>
-                : null }
+            <ul className="duties">
+                <li className="duties">{dutiesOne}</li>
+                <li className="duties">{dutiesTwo}</li>
+                <li className="duties">{dutiesThree}</li>
+            </ul>
+        </div>
 
-                { jobThree ?
-                    <div className={jobThree}>
-                        <h4 className="title">{newTitleThree}</h4>
-                        <p className="company">{newCompanyThree}</p>
-                        <p className="date">{newDateThree}</p>
+    { jobTwo ?
+        <div className={jobTwo}>
+            <h4 className="title">{newTitleTwo}</h4>
+            <p className="company">{newCompanyTwo}</p>
+            <p className="date">{newDateTwo}</p>
 
-                        <ul className="duties">
-                            <li className="duties">{newDutiesThreeOne}</li>
-                            <li className="duties">{newDutiesThreeTwo}</li>
-                            <li className="duties">{newDutiesThreeThree}</li>
-                        </ul>
-                    </div>
-                : null }
+            <ul className="duties">
+                <li className="duties">{newDutiesTwoOne}</li>
+                <li className="duties">{newDutiesTwoTwo}</li>
+                <li className="duties">{newDutiesTwoThree}</li>
+            </ul>
+        </div>
+    : null }
 
-            </section>
-        )
-    }
+    { jobThree ?
+        <div className={jobThree}>
+            <h4 className="title">{newTitleThree}</h4>
+            <p className="company">{newCompanyThree}</p>
+            <p className="date">{newDateThree}</p>
+
+            <ul className="duties">
+                <li className="duties">{newDutiesThreeOne}</li>
+                <li className="duties">{newDutiesThreeTwo}</li>
+                <li className="duties">{newDutiesThreeThree}</li>
+            </ul>
+        </div>
+    : null }
+
+    </section>
+  )
 }
 
 export default ProfExp
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ProfExp from './ProfExp'
 
-class Resume extends Component {    
-    render() {
-        const {jobOne, jobTwo, jobThree, firstName, lastName, address, city, state, zip, email, phone, school, degree, year, honors, gpa, company, title, dutiesOne, dutiesTwo, dutiesThree, date, newCompanyTwo, newTitleTwo, newDutiesTwoOne, newDutiesTwoTwo, newDutiesTwoThree, newDateTwo, newCompanyThree, newTitleThree, newDutiesThreeOne, newDutiesThreeTwo, newDutiesThreeThree, newDateThree} = this.props
+const Resume = ({jobOne, jobTwo, jobThree, firstName, lastName, address, city, state, zip, email, phone, school, degree, year, honors, gpa, company, title, dutiesOne, dutiesTwo, dutiesThree, date, newCompanyTwo, newTitleTwo, newDutiesTwoOne, newDutiesTwoTwo, newDutiesTwoThree, newDateTwo, newCompanyThree, newTitleThree, newDutiesThreeOne, newDutiesThreeTwo, newDutiesThreeThree, newDateThree}) => {
+  return (
+    <div className="resume">
+        <h2 className="name">{firstName} {lastName}</h2>
 
-        return (
-            <div className="resume">
-                <h2 className="name">{firstName} {lastName}</h2>
-                
-                <div className="divider"></div>
-                
-                <section className="general-info">
-                    <p className="address">{address}</p>
-                    <p className="city-state">{city} {state} {zip}</p>
-                    <p className="email">{email}</p>
-                    <p className="phone">{phone}</p>
-                </section>
+        <div className="divider"></div>
 
-                <ProfExp jobOne={jobOne} company={company} title={title} dutiesOne={dutiesOne} dutiesTwo={dutiesTwo} dutiesThree={dutiesThree} date={date} jobTwo={jobTwo} jobThree={jobThree} newCompanyTwo={newCompanyTwo} newTitleTwo={newTitleTwo} newDutiesTwoOne={newDutiesTwoOne} newDutiesTwoTwo={newDutiesTwoTwo} newDutiesTwoThree={newDutiesTwoThree} newDateTwo={newDateTwo} newCompanyThree={newCompanyThree} newTitleThree={newTitleThree} newDutiesThreeOne={newDutiesThreeOne} newDutiesThreeTwo={newDutiesThreeTwo} newDutiesThreeThree={newDutiesThreeThree} newDateThree={newDateThree}/>
+        <section className="general-info">
+            <p className="address">{address}</p>
+            <p className="city-state">{city} {state} {zip}</p>
+            <p className="email">{email}</p>
+            <p className="phone">{phone}</p>
+        </section>
 
-                <section className="education">
-                    <h3>Education</h3>
-                    <p className="school">{school}</p>
-                    <p className="degree">{degree}, ({year})</p>
-                    <p className="honors">{honors}</p>
-                    <p className="gpa">GPA: {gpa}</p>
-                </section>
-            </div>
-        )
-    }
+        <ProfExp jobOne={jobOne} company={company} title={title} dutiesOne={dutiesOne} dutiesTwo={dutiesTwo} dutiesThree={dutiesThree} date={date} jobTwo={jobTwo} jobThree={jobThree} newCompanyTwo={newCompanyTwo} newTitleTwo={newTitleTwo} newDutiesTwoOne={newDutiesTwoOne} newDutiesTwoTwo={newDutiesTwoTwo} newDutiesTwoThree={newDutiesTwoThree} newDateTwo={newDateTwo} newCompanyThree={newCompanyThree} newTitleThree={newTitleThree} newDutiesThreeOne={newDutiesThreeOne} newDutiesThreeTwo={newDutiesThreeTwo} newDutiesThreeThree={newDutiesThreeThree} newDateThree={newDateThree}/>
+
+        <section className="education">
+            <h3>Education</h3>
+            <p className="school">{school}</p>
+            <p className="degree">{degree}, ({year})</p>
+            <p className="honors">{honors}</p>
+            <p className="gpa">GPA: {gpa}</p>
+        </section>
+    </div>
+  )
 }
 
 export default Resume
