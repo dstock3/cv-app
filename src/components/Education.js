0974610b012f94ad8ed27d228424d2fa3f1ac5aa
David Stockdale <dstock3@gmail.com>
2022-02-10 22:01:24 -0600

    Consolidated honors and gpa on the same line in Education component.

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,11 +4,12 @@ const Education = ({school, degree, major, year, honors, gpa}) => {
   return (
     <div className="education">
         <h3>Education</h3>
+
         { school ? <p className="school">{school}</p> : null }
         { degree ? <p className="degree">{degree}, ({year})</p> : null }
         { major ? <p className="major">{major}</p> : null }
-        { honors ? <p className="honors">{honors}</p> : null }
-        { gpa ? <p className="gpa">GPA: {gpa}</p> : null }
+        { gpa ? <p className="honors">{honors}, GPA: {gpa}</p> : null }
+        
     </div>
   )
 }
