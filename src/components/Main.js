517c987e89a1b90156fba8e3af8bf60c2268abac
David Stockdale <dstock3@gmail.com>
2022-02-21 15:38:03 -0600

    Initialized states for EdForm which are passed from Main.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
+import EdForm from './EdForm'
 import GenForm from './GenForm'
 
 const Main = () => {
     const [genInfo, setGenInfo] = useState({firstName: '', lastName: '', address: '', zip: '', zip: '', city: '', state: '', email: '', phone: '', genClass: ''})
 
+    const [edInfo, setEdInfo] = useState({school: '', degree: '', year: '', honors: '', gpa: ''})
+
     return (
         <main className="section-container">
             <section className="form-container">
                 <h1>Resume Builder</h1>
+
                 <form className="resume-form">
+
                     <GenForm genInfo={genInfo} setGenInfo={setGenInfo}/>
+
+                    <EdForm edInfo={edInfo} setEdInfo={setEdInfo} />
 
 
                 </form>
