b46b37373d5e3f6e4926c61fa08db79c99742522
David Stockdale <dstock3@gmail.com>
2022-02-21 15:59:17 -0600

    Added placeholder values to EdForm component.

--- src/components/EdForm.js
+++ src/components/EdForm.js
@@ -1,9 +1,27 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const EdForm = ({edInfo, setEdInfo}) => {
     const [edClass, setEdClass] = useState('')
 
+    const [placeholders, setPlaceholders] = useState({
+        school: "University", 
+        degree: "Degree", 
+        year: "Graduation Date",
+        honors: "Honors",
+        gpa: "Grade Point Average"
+    })
+
     const addEdInfo = () => {
+        if (edInfo.school && edInfo.degree && edInfo.year) {
+            console.log(edInfo)
+        } else {
+            setEdClass("error")
+            setPlaceholders({
+                school: "Required Field",
+                degree: "Required Field",
+                year: "Required Field"
+            })
+        }
 
     }
 
@@ -13,20 +31,20 @@ const EdForm = ({edInfo, setEdInfo}) => {
             <div className="education-container">
                 <div className="education-subcontainer-left">
                     <label htmlFor="school">School:</label>
-                    <input className={edClass} type="text" value={school} name="school" placeholder={schoolPlaceholder} onChange={e => setEdInfo({...edInfo, school: e.target.value})} />
+                    <input className={edClass} type="text" value={edInfo.school} name="school" placeholder={placeholders.school} onChange={e => setEdInfo({...edInfo, school: e.target.value})} />
 
                     <label htmlFor="degree">Degree:</label>
-                    <input className={edClass} type="text" value={degree} name="degree" placeholder={degreePlaceholder} onChange={e => setEdInfo({...edInfo, degree: e.target.value})} />
+                    <input className={edClass} type="text" value={edInfo.degree} name="degree" placeholder={placeholders.degree} onChange={e => setEdInfo({...edInfo, degree: e.target.value})} />
 
                     <label htmlFor="year">Year of Completion:</label>
-                    <input className={edClass} type="text" value={year} name="year" placeholder={yearPlaceholder} onChange={e => setEdInfo({...edInfo, year: e.target.value})} />
+                    <input className={edClass} type="text" value={edInfo.year} name="year" placeholder={placeholders.year} onChange={e => setEdInfo({...edInfo, year: e.target.value})} />
                 </div>
                 <div className="education-subcontainer-right">
                     <label htmlFor="honors">Honors:</label>
-                    <input type="text" value={honors} name="honors" placeholder='Honors' onChange={this.changeHonorsHandler} />
+                    <input type="text" value={edInfo.honors} name="honors" placeholder={placeholders.honors} onChange={e => setEdInfo({...edInfo, honors: e.target.value})} />
 
                     <label htmlFor="gpa">GPA:</label>
-                    <input className={edClass} type="text" value={gpa} name="gpa" placeholder={gpaPlaceholder} onChange={e => setEdInfo({...edInfo, gpa: e.target.value})} />
+                    <input className={edClass} type="text" value={edInfo.gpa} name="gpa" placeholder={placeholders.gpa} onChange={e => setEdInfo({...edInfo, gpa: e.target.value})} />
                 </div>
             </div>
 
