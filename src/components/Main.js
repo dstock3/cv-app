b11e25442c62e6ab08e35d99c82ad9bb768d93b0
David Stockdale <dstock3@gmail.com>
2022-02-25 17:18:27 -0600

    Added removeInfo method to Main component. Added Resume component and revised the props that are passed to it.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import EdForm from './EdForm'
 import GenForm from './GenForm'
 import ProfForm from './ProfForm'
+import Resume from './Resume'
+import Favicon from '../assets/favicon.png'
 
 const Main = () => {
-    const [genInfo, setGenInfo] = useState({firstName: '', lastName: '', address: '', zip: '', zip: '', city: '', state: '', email: '', phone: '', genClass: ''})
+    useEffect(()=> {
+        const head = document.querySelector("head");
+        const favIcon = document.createElement("link")
+        head.appendChild(favIcon)
+        favIcon.rel = "shortcut icon";
+        favIcon.type = "image/png";
+        favIcon.href = Favicon
+    })
+
+    const [genInfo, setGenInfo] = useState({firstName: '', lastName: '', address: '', zip: '', city: '', state: '', email: '', phone: '', genClass: ''})
 
     const [edInfo, setEdInfo] = useState({school: '', degree: '', year: '', honors: '', gpa: ''})
 
     const [profInfo, setProfInfo] = useState({company: '', title: '',dutiesOne: '', dutiesTwo: '', dutiesThree: '', date: ''})
+
+    const removeInfo = () => {
+        setGenInfo({firstName: '', lastName: '', address: '', zip: '', city: '', state: '', email: '', phone: '', genClass: ''})
+    }
+
+    const handleSubmit = () => {
+
+    }
 
     return (
         <main className="section-container">
             <section className="form-container">
                 <h1>Resume Builder</h1>
 
-                <form className="resume-form">
+                <form className="resume-form" onSubmit={handleSubmit}>
                     <GenForm genInfo={genInfo} setGenInfo={setGenInfo}/>
 
                     <EdForm edInfo={edInfo} setEdInfo={setEdInfo} />
 
                     <ProfForm profInfo={profInfo} setProfInfo={setProfInfo} />
+
+                    <div className="button-container">
+                        <button className="submit" type="submit">Submit</button>
+                        <button className="remove" onClick={removeInfo}>Clear</button>
+                    </div>
                 </form>
+            </section>
+            <section className="resume-container">
+                <div className="preview-head-container">
+                    <h2 className="preview">Preview</h2>
+                </div>
+                
+                <Resume genInfo={genInfo} edInfo={edInfo} profInfo={profInfo}/>
             </section>
         </main>
     )
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,32 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import ProfExp from './ProfExp'
 import GenInfo from './GenInfo'
 import Education from './Education'
 import Divider from './Divider'
-import Favicon from '../assets/favicon.png'
 
-const Resume = ({jobOne, jobTwo, jobThree, firstName, lastName, address, city, state, zip, email, phone, school, degree, major, year, honors, gpa, company, title, dutiesOne, dutiesTwo, dutiesThree, date, newCompanyTwo, newTitleTwo, newDutiesTwoOne, newDutiesTwoTwo, newDutiesTwoThree, newDateTwo, newCompanyThree, newTitleThree, newDutiesThreeOne, newDutiesThreeTwo, newDutiesThreeThree, newDateThree}) => {
-
-    useEffect(()=> {
-        const head = document.querySelector("head");
-        const favIcon = document.createElement("link")
-        head.appendChild(favIcon)
-        favIcon.rel = "shortcut icon";
-        favIcon.type = "image/png";
-        favIcon.href = Favicon
-    })
+const Resume = ({genInfo, edInfo, profInfo}) => {
 
     return (
         <div className="resume">
-            <h2 className="name">{firstName} {lastName}</h2>
+            <h2 className="name">{genInfo.firstName} {genInfo.lastName}</h2>
 
             <Divider />
 
-            <GenInfo address={address} city={city} state={state} zip={zip} email={email} phone={phone} />
+            <GenInfo address={genInfo.address} city={genInfo.city} state={genInfo.state} zip={genInfo.zip} email={genInfo.email} phone={genInfo.phone} />
 
-            <ProfExp jobOne={jobOne} company={company} title={title} dutiesOne={dutiesOne} dutiesTwo={dutiesTwo} dutiesThree={dutiesThree} date={date} jobTwo={jobTwo} jobThree={jobThree} newCompanyTwo={newCompanyTwo} newTitleTwo={newTitleTwo} newDutiesTwoOne={newDutiesTwoOne} newDutiesTwoTwo={newDutiesTwoTwo} newDutiesTwoThree={newDutiesTwoThree} newDateTwo={newDateTwo} newCompanyThree={newCompanyThree} newTitleThree={newTitleThree} newDutiesThreeOne={newDutiesThreeOne} newDutiesThreeTwo={newDutiesThreeTwo} newDutiesThreeThree={newDutiesThreeThree} newDateThree={newDateThree}/>
-            
-            <Education school={school} major={major} degree={degree} year={year} honors={honors} gpa={gpa} />
+            <ProfExp jobOne={profInfo.jobOne} company={profInfo.company} title={profInfo.title} dutiesOne={profInfo.dutiesOne} dutiesTwo={profInfo.dutiesTwo} dutiesThree={profInfo.dutiesThree} date={profInfo.date}/>
+
+            <Education school={edInfo.school} major={edInfo.major} degree={edInfo.degree} year={edInfo.year} honors={edInfo.honors} gpa={edInfo.gpa} />
         </div>
     )
 }
